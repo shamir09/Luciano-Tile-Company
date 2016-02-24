e9ca349c1aaa89db86fae862f0a156e2acbcf6a9
Shamir Ajate <shamirajate@gmail.com>
2016-02-23 20:29:02 -0500

    Pricing on Flooring Type

--- Luciano_tile_company_Jquery.js
+++ Luciano_tile_company_Jquery.js
@@ -3,7 +3,7 @@
    function calculateCost() {
               var width = $("#w").val();
               var length = $("#l").val();
-              var sqft = (width * length).toFixed(1);
+              var sqft = (width * length).toFixed(2);
               var cost = (sqft * 2.93).toFixed(2);
 
               $("#sqfOutput").text(sqft + " sqft" || 0);
@@ -20,6 +20,19 @@
                 $("#w").focus();  /* Focus to Width Bar */
                 $("#w").select();
                           }
+                          /* Hide the Logo */
+              $("#logo, #sqfOutput, #cost, #infosqf, #header, #floorsContainer").hide();
+
+                          /* Fade in the Logo */
+              $("#logo").show(3000);
+              $("#header").fadeIn(2000);
+              $("#floorsContainer").fadeIn(3000);
+              $("#w").focus();       /* Focus to Width Bar */
+                                              /* On leave Calculate the Cost */
+              $("#l").on("blur",function() {
+              calculateCost();
+              });
+                        
 
    document.onkeydown = function (evt) {
        evt = evt || window.event;
@@ -30,20 +43,4 @@
            calculateCost();
            }
    };
-                            /* Hide the Logo */
-     $("#logo, #sqfOutput, #cost, #infosqf, #header, #floorsContainer").hide();
-
-                            /* Fade in the Logo */
-     $("#logo").show(3000);
-     $("#header").fadeIn(4750);
-     $("#floorsContainer").fadeIn(10000);
-     $("#w").focus();       /* Focus to Width Bar */
-                                                /* On leave Calculate the Cost */
-            $("#l").on("blur",function() {
-              calculateCost();
-                });
-                                                /* On Focus Select All   */
-            $("#w").on("focus",function() {
-               clearFields();
-                });
 });
